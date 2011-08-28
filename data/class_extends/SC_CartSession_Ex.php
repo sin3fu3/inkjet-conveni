2cbbf14492aed086e8c486b23078d2aa578d0338
sin3fu3 <shin@webuya.com>
2011-08-28 17:38:34 +0900

    数量の小数点許可対応

--- data/class_extends/SC_CartSession_Ex.php
+++ data/class_extends/SC_CartSession_Ex.php
@@ -24,6 +24,46 @@
 require_once CLASS_REALDIR . 'SC_CartSession.php';
 
 class SC_CartSession_Ex extends SC_CartSession {
+
+	// 数量の増加
+    function upQuantity($cart_no, $productTypeId) {
+        $quantity = $this->getQuantity($cart_no, $productTypeId);
+        // & hatanaka 2011.08.28
+        if (strlen($quantity + 0.1) <= INT_LEN) {
+            $this->setQuantity($quantity + 0.1, $cart_no, $productTypeId);
+            // hatanaka & 2011.08.28
+        }
+    }
+
+    // 数量の減少
+    function downQuantity($cart_no, $productTypeId) {
+        $quantity = $this->getQuantity($cart_no, $productTypeId);
+        // & hatanaka 2011.08.28
+        if ($quantity > 0.1) {
+            $this->setQuantity($quantity - 0.1, $cart_no, $productTypeId);
+            // hatanaka & 2011.08.28
+        }
+    }
+
+    /**
+     * カートが保持するキー(商品種別ID)を配列で返す.
+     *
+     * @return array 商品種別IDの配列
+     */
+    function getKeys() {
+        $keys = array_keys($this->cartSession);
+        // 数量が 0 の商品種別は削除する
+        foreach ($keys as $key) {
+            $quantity = $this->getTotalQuantity($key);
+            // & hatanaka 2011.08.28
+            if ($quantity < 0.1) {
+            	// hatanaka & 2011.08.28
+                unset($this->cartSession[$key]);
+            }
+        }
+        return array_keys($this->cartSession);
+    }
+
 }
 
 ?>
