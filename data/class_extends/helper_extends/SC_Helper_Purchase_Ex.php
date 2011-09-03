cf65524fb4715658431b14196a26e36044273379
sin3fu3 <shin@webuya.com>
2011-09-03 10:09:47 +0900

    会社名と部署名の追加

--- data/class_extends/helper_extends/SC_Helper_Purchase_Ex.php
+++ data/class_extends/helper_extends/SC_Helper_Purchase_Ex.php
@@ -34,5 +34,81 @@ require_once CLASS_REALDIR . 'helper/SC_Helper_Purchase.php';
  * @version $Id: SC_Helper_Purchase_Ex.php 20764 2011-03-22 06:26:40Z nanasess $
  */
 class SC_Helper_Purchase_Ex extends SC_Helper_Purchase {
+
+	/**
+     * 会員情報を受注情報にコピーする.
+     *
+     * ユーザーがログインしていない場合は何もしない.
+     * 会員情報を $dest の order_* へコピーする.
+     * customer_id は強制的にコピーされる.
+     *
+     * @param array $dest コピー先の配列
+     * @param SC_Customer $objCustomer SC_Customer インスタンス
+     * @param string $prefix コピー先の接頭辞. デフォルト order
+     * @param array $keys コピー対象のキー
+     * @return void
+     */
+    function copyFromCustomer(&$dest, &$objCustomer, $prefix = 'order',
+                              $keys = array('name01', 'name02', 'kana01', 'kana02',
+                                            'sex', 'zip01', 'zip02', 'pref',
+                                            'addr01', 'addr02',
+                                            'tel01', 'tel02', 'tel03', 'job',
+                                            'birth', 'email', 'company_name', 'section_name')) {
+        if ($objCustomer->isLoginSuccess(true)) {
+
+            foreach ($keys as $key) {
+                if (in_array($key, $keys)) {
+                    $dest[$prefix . '_' . $key] = $objCustomer->getValue($key);
+                }
+            }
+
+            if (Net_UserAgent_Mobile::isMobile()
+                && in_array('email', $keys)) {
+                $email_mobile = $objCustomer->getValue('email_mobile');
+                if (empty($email_mobile)) {
+                    $dest[$prefix . '_email'] = $objCustomer->getValue('email');
+                } else {
+                    $dest[$prefix . '_email'] = $email_mobile;
+                }
+            }
+
+            $dest['customer_id'] = $objCustomer->getValue('customer_id');
+            $dest['update_date'] = 'Now()';
+        }
+    }
+
+    /**
+     * 受注情報を配送情報にコピーする.
+     *
+     * 受注情報($src)を $dest の order_* へコピーする.
+     *
+     * TODO 汎用的にして SC_Utils へ移動
+     *
+     * @param array $dest コピー先の配列
+     * @param array $src コピー元の配列
+     * @param array $keys コピー対象のキー
+     * @param string $prefix コピー先の接頭辞. デフォルト shipping
+     * @param string $src_prefix コピー元の接頭辞. デフォルト order
+     * @return void
+     */
+    function copyFromOrder(&$dest, $src,
+                           $prefix = 'shipping', $src_prefix = 'order',
+                           $keys = array('name01', 'name02', 'kana01', 'kana02',
+                                         'sex', 'zip01', 'zip02', 'pref',
+                                         'addr01', 'addr02',
+                                         'tel01', 'tel02', 'tel03', 'company_name', 'section_name')) {
+        if (!SC_Utils_Ex::isBlank($prefix)) {
+            $prefix = $prefix . '_';
+        }
+        if (!SC_Utils_Ex::isBlank($src_prefix)) {
+            $src_prefix = $src_prefix . '_';
+        }
+        foreach ($keys as $key) {
+            if (in_array($key, $keys)) {
+                $dest[$prefix . $key] = $src[$src_prefix . $key];
+            }
+        }
+    }
+
 }
 ?>
